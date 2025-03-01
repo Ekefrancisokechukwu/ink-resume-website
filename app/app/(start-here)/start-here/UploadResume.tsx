9c40d821bc77713a9e28d5cb6bab22441011053e
Francis Okechukwu <113605646+Ekefrancisokechukwu@users.noreply.github.com>
2025-03-01 08:20:52 +0100

    update:

--- app/app/(start-here)/start-here/UploadResume.tsx
+++ app/app/(start-here)/start-here/UploadResume.tsx
@@ -1,6 +1,9 @@
 import { UI } from "@/components/ui";
 import { CloudUpload } from "lucide-react";
 import { FaFilePdf } from "react-icons/fa6";
+import { FaCheckCircle } from "react-icons/fa";
+
+// import {motion} from ""ion
 
 const UploadResumeModal = () => {
   return (
@@ -20,31 +23,38 @@ const UploadResumeModal = () => {
         </div>
 
         <div className="mt-5">
-          <div className="border text-primary-black rounded-[13px] px-6 py-5">
+          <div className="shadow-fade text-primary-black rounded-[13px] p-5">
             <div className="flex gap-x-5">
               <div>
                 <FaFilePdf color="#3d3d3d" size={24} />
               </div>
               <div className="grow">
                 <div className="flex items-center justify-between">
-                  <p>2024-my-portfolio.pdf</p>
-                  <div>completed</div>
+                  <p className="text-neutral-600">2024-my-portfolio.pdf</p>
+                  <div className="flex items-center gap-x-2 ">
+                    {/* <FaCheckCircle color="#3d3d" />
+                    <span className="font-medium text-primary-black text-sm">
+                      completed
+                    </span> */}
+                    <span className="font-semibold text-neutral-500 text-sm">
+                      98%
+                    </span>
+                  </div>
                 </div>
-                <div className="shadow-inner mt-2 h-3 border rounded"></div>
+                <div className="shadow-inner mt-2 h-2.5 border bg-neutral-100 rounded">
+                  <div className="h-full bg-primary-black rounded w-7 transition-all duration-300" />
+                </div>
+
+                <div className="flex items-center mt-3 gap-x-3">
+                  <button className="bg-neutral-100 hover:bg-neutral-200 transition-colors duration-500 font-medium px-3 py-1 text-neutral-400 rounded-[10px]">
+                    Change
+                  </button>
+                  <button className="bg-red-50 hover:bg-red-100 text-red-400 font-medium px-3 py-1 transition-colors duration-500 rounded-[10px]">
+                    Remove
+                  </button>
+                </div>
               </div>
             </div>
-
-            {/* <div className="flex items-center justify-center gap-x-5">
-              <UI.Button size={"sm"} variant={"secondary"}>
-                Change
-              </UI.Button>
-              <UI.Button
-                size={"sm"}
-                className="bg-red-200 text-red-700 hover:bg-transparent"
-              >
-                Remove
-              </UI.Button>
-            </div> */}
           </div>
         </div>
       </div>
