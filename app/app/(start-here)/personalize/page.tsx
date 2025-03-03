cf0e1312f2dc1786a760eb2201f1cc1b59e18047
Francis Okechukwu <113605646+Ekefrancisokechukwu@users.noreply.github.com>
2025-03-03 06:14:03 +0100

    personal info done

--- app/app/(start-here)/personalize/page.tsx
+++ app/app/(start-here)/personalize/page.tsx
@@ -2,7 +2,7 @@
 
 import { UI } from "@/components/ui";
 import { StepperNav } from "./StepperNav";
-import { Check, ChevronsUpDown, CircleUser, Plus } from "lucide-react";
+import { Check, ChevronsUpDown, CircleUser, Plus, Trash } from "lucide-react";
 import { countries } from "@/constant";
 import React from "react";
 import { cn } from "@/lib/utils";
@@ -11,6 +11,20 @@ import { motion } from "motion/react";
 const personalLinks = ["linkedin", "x", "website", "github", "behance"];
 
 const PersonalizePage = () => {
+  return (
+    <div className="bg-neutral-50 min-h-screen grid grid-cols-[auto_1fr_auto] md:pl-0 px-5">
+      <StepperNav />
+      <section className="grid place-items-center pt-10 pb-24">
+        <div className="max-w-[30rem] w-full ">
+          <PersonalInformation />
+        </div>
+      </section>
+      <div className="w-[13rem] md:block hidden"></div>
+    </div>
+  );
+};
+
+const PersonalInformation = () => {
   const [open, setOpen] = React.useState(false);
   const [value, setValue] = React.useState("");
   const [selectedLink, setSelectedLink] = React.useState<string[] | []>([]);
@@ -20,114 +34,128 @@ const PersonalizePage = () => {
   );
 
   return (
-    <div className="bg-neutral-50 min-h-screen grid grid-cols-[auto_1fr_auto] md:pl-0 px-5">
-      <StepperNav />
-      <section className="grid place-items-center py-10">
-        <div className="max-w-[30rem] w-full ">
-          <UI.Heading text="Personal Information" />
-          <Photo />
-          <div className="mt-7 space-y-4">
-            <div className="grid grid-cols-2 gap-x-6 ">
-              <UI.Input label="Full Name" />
-              <UI.Input label="Job Title" />
-            </div>
-            <UI.Input label="Phone" type="tel" />
-            <div className="grid grid-cols-2 gap-x-6 ">
-              <div className="flex flex-col gap-y-1.5">
-                <UI.Label>Country</UI.Label>
-                <UI.Popover open={open} onOpenChange={setOpen}>
-                  <UI.PopoverTrigger asChild>
-                    <UI.Button
-                      variant="outline"
-                      role="combobox"
-                      aria-expanded={open}
-                      className="justify-between h-8 rounded-lg"
-                    >
-                      {value
-                        ? countries.find((country) => country.label === value)
-                            ?.label
-                        : "Select country..."}
-                      <ChevronsUpDown className="opacity-50" />
-                    </UI.Button>
-                  </UI.PopoverTrigger>
-                  <UI.PopoverContent className="w-[200px] p-0 rounded-lg">
-                    <UI.Command>
-                      <UI.CommandInput placeholder="Search framework..." />
-                      <UI.CommandList className="scroll-bar">
-                        <UI.CommandEmpty>No country found.</UI.CommandEmpty>
-                        <UI.CommandGroup>
-                          {countries.map((country) => (
-                            <UI.CommandItem
-                              key={country.value}
-                              value={country.label}
-                              onSelect={(currentValue) => {
-                                setValue(
-                                  currentValue === value ? "" : currentValue
-                                );
-                                setOpen(false);
-                              }}
-                            >
-                              {country.label}
-                              <Check
-                                className={cn(
-                                  "ml-auto",
-                                  value === country.label
-                                    ? "opacity-100"
-                                    : "opacity-0"
-                                )}
-                              />
-                            </UI.CommandItem>
-                          ))}
-                        </UI.CommandGroup>
-                      </UI.CommandList>
-                    </UI.Command>
-                  </UI.PopoverContent>
-                </UI.Popover>
-              </div>
-              <div className="flex flex-col gap-y-1.5">
-                <UI.Input label="City" />
-              </div>
-            </div>
-            <UI.Input label="Email" type="email" />
-
-            {selectedLink.length > 0 && (
-              <div className="grid grid-cols-2 gap-x-6 gap-y-4 ">
-                {[...selectedLink].map((selected) => {
-                  return (
-                    <motion.div
-                      key={selected}
-                      initial={{ y: 10 }}
-                      animate={{ y: 0 }}
-                      transition={{ duration: 0.3, ease: "easeOut" }}
-                    >
-                      <UI.Input label={selected} type="text" />
-                    </motion.div>
-                  );
-                })}
-              </div>
-            )}
-
-            <div className="flex items-center flex-wrap gap-3 ">
-              {avaliableLinks.map((link) => {
-                return (
-                  <button
-                    onClick={() =>
-                      setSelectedLink((prev) => {
-                        return [...prev, link];
-                      })
-                    }
-                    key={link}
-                    className="border font-semibold capitalize inline-flex items-center  gap-x-2 ring-neutral-400 px-3 py-1 rounded-full  transition-colors duration-300  text-neutral-500"
-                  >
-                    {link} <Plus size={18} />
-                  </button>
-                );
-              })}
-            </div>
+    <div>
+      <UI.Heading text="Personal Information" />
+      <Photo />
+      <div className="mt-7 space-y-4">
+        <div className="grid grid-cols-2 gap-x-6 ">
+          <UI.Input label="Full Name" />
+          <UI.Input label="Job Title" />
+        </div>
+        <UI.Input label="Phone" type="tel" />
+        <div className="grid grid-cols-2 gap-x-6 ">
+          <div className="flex flex-col gap-y-1.5">
+            <UI.Label>Country</UI.Label>
+            <UI.Popover open={open} onOpenChange={setOpen}>
+              <UI.PopoverTrigger asChild>
+                <UI.Button
+                  variant="outline"
+                  role="combobox"
+                  aria-expanded={open}
+                  className="justify-between h-8 rounded-lg"
+                >
+                  {value
+                    ? countries.find((country) => country.label === value)
+                        ?.label
+                    : "Select country..."}
+                  <ChevronsUpDown className="opacity-50" />
+                </UI.Button>
+              </UI.PopoverTrigger>
+              <UI.PopoverContent className="w-[200px] p-0 rounded-lg">
+                <UI.Command>
+                  <UI.CommandInput placeholder="Search framework..." />
+                  <UI.CommandList className="scroll-bar">
+                    <UI.CommandEmpty>No country found.</UI.CommandEmpty>
+                    <UI.CommandGroup>
+                      {countries.map((country) => (
+                        <UI.CommandItem
+                          key={country.value}
+                          value={country.label}
+                          onSelect={(currentValue) => {
+                            setValue(
+                              currentValue === value ? "" : currentValue
+                            );
+                            setOpen(false);
+                          }}
+                        >
+                          {country.label}
+                          <Check
+                            className={cn(
+                              "ml-auto",
+                              value === country.label
+                                ? "opacity-100"
+                                : "opacity-0"
+                            )}
+                          />
+                        </UI.CommandItem>
+                      ))}
+                    </UI.CommandGroup>
+                  </UI.CommandList>
+                </UI.Command>
+              </UI.PopoverContent>
+            </UI.Popover>
+          </div>
+          <div className="flex flex-col gap-y-1.5">
+            <UI.Input label="City" />
           </div>
         </div>
-      </section>
-      <div className="w-[13rem] md:block hidden"></div>
+        <UI.Input label="Email" type="email" />
+
+        {selectedLink.length > 0 && (
+          <motion.div layoutRoot className="grid grid-cols-2 gap-x-6 gap-y-4 ">
+            {[...selectedLink].map((selected) => {
+              return (
+                <motion.div
+                  key={selected}
+                  layoutId={selected}
+                  initial={{ y: 10 }}
+                  animate={{ y: 0 }}
+                  transition={{ duration: 0.3, ease: "easeOut" }}
+                >
+                  <UI.Input
+                    label={selected}
+                    type="text"
+                    rightIcon={
+                      <button
+                        className="group"
+                        onClick={() =>
+                          setSelectedLink((links) =>
+                            links.filter((link) => link !== selected)
+                          )
+                        }
+                      >
+                        <Trash className="text-neutral-400 group-hover:text-neutral-700" />
+                      </button>
+                    }
+                  />
+                </motion.div>
+              );
+            })}
+          </motion.div>
+        )}
+
+        <div className="flex items-center flex-wrap gap-3 ">
+          {avaliableLinks.map((link) => {
+            return (
+              <button
+                onClick={() =>
+                  setSelectedLink((prev) => {
+                    return [...prev, link];
+                  })
+                }
+                key={link}
+                className="border font-semibold capitalize inline-flex items-center  gap-x-2 ring-neutral-400 px-3 py-1 rounded-full  transition-colors duration-300  text-neutral-500"
+              >
+                {link} <Plus size={18} />
+              </button>
+            );
+          })}
+        </div>
+      </div>
+      <div className="mt-8 flex justify-between items-center">
+        <div />
+        <UI.Button>Save and Continue</UI.Button>
+      </div>
     </div>
   );
 };
--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -2,27 +2,35 @@ import * as React from "react";
 
 import { cn } from "@/lib/utils";
 import { Label } from "./label";
+import { Trash } from "lucide-react";
 
 interface InputProps extends React.ComponentProps<"input"> {
   label?: string;
+  rightIcon?: React.ReactNode;
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, label, id, ...props }, ref) => {
+  ({ className, type, label, id, rightIcon, ...props }, ref) => {
     return (
       <div className="flex flex-col gap-y-1.5">
         {label && <Label htmlFor={label}>{label}</Label>}
 
-        <input
-          type={type}
-          className={cn(
-            "flex h-8 transition-all duration-200 w-full rounded-lg border border-input bg-background px-3 py-2 text-base ring-offset-accent file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-500 focus-visible:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-            className
-          )}
-          ref={ref}
-          id={id || label}
-          {...props}
-        />
+        <div className="relative">
+          <div className="absolute top-1/2  -translate-y-1/2 right-2 [&_svg]:size-4">
+            {rightIcon}
+          </div>
+          <input
+            type={type}
+            className={cn(
+              "flex h-8 transition-all  duration-200 w-full rounded-lg border border-input bg-background px-3 py-2 text-base ring-offset-accent file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-500 focus-visible:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+              className,
+              rightIcon ? "!pr-7" : ""
+            )}
+            ref={ref}
+            id={id || label}
+            {...props}
+          />
+        </div>
       </div>
     );
   }
